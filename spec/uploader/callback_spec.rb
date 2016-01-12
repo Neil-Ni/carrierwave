f17ee898cf76866ada74fa0ee9f7170ce8f7c5d1
Neil-Ni <tzuyangni@gmail.com>
2016-01-12 16:05:03 -0500

    added failed spec for original_filename check before callback

--- spec/uploader/callback_spec.rb
+++ spec/uploader/callback_spec.rb
@@ -26,5 +26,22 @@ describe CarrierWave::Uploader do
     expect(@uploader_class_1._before_callbacks[:cache]).to eq(default_before_callbacks)
   end
 
+  context 'when before cache callback exists' do
+    before do
+      @uploader_class = Class.new(CarrierWave::Uploader::Base)
+      @uploader = @uploader_class.new
+      @file = File.open(file_path('test.jpg'))
+      @sanitized_file = CarrierWave::SanitizedFile.new(@file)
+      allow(@sanitized_file).to receive(:original_filename).at_least(:once).and_return("test-s,%&m#st?.jpg")
+      allow(@uploader).to receive(:before_cache_callback)
+      @uploader_class.before :cache, :before_cache_callback
+      @uploader.cache!(@sanitized_file)
+    end
 
+    it "returns original file with unsanitize filename" do
+      expect(@uploader).to have_received(:before_cache_callback) do |arg|
+        expect(arg.original_filename).to eq "test-s,%&m#st?.jpg"
+      end
+    end
+  end
 end
